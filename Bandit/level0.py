e3a7df9a0e8c0b42865d15b47659064f1d446a9a
Ubuntu <ubuntu@ip-172-31-30-28.ap-south-1.compute.internal>
2017-10-11 02:00:04 +0000

    level1 solution added

--- Bandit/level0.py
+++ Bandit/level0.py
@@ -1,8 +1,10 @@
 from pwn import *
 
-shell = ssh(host='bandit.labs.overthewire.org', user='bandit0', password='bandit0', port=2220)
+host = 'bandit.labs.overthewire.org'
+username = 'bandit0'
+password = 'bandit0'
 
-log.info("username: %s" % shell.whoami())
-log.info("pwd: %s" % shell.pwd())
-
-print shell['cat readme']
+shell = ssh(host = host, user = username, password = password, port = 2220)
+log.info("List dir: %s\n" % shell.ls(['-la']))
+log.success("Password: %s" % shell.cat(['readme']))
+shell.close()
