98910f3ab562f8740c7d646d12e7ded25bb050ba
Ubuntu <ubuntu@ip-172-31-30-28.ap-south-1.compute.internal>
2017-10-12 06:40:56 +0000

    Bandit level2 solution added

---
+++ Bandit/level2.py
@@ -0,0 +1,18 @@
+import sys
+from pwn import *
+
+user = 'bandit2'
+host = 'bandit.labs.overthewire.org'
+
+if len(sys.argv) != 2:
+	log.failure("Usage: %s <%s password>" % __file__, user)
+	sys.exit()
+
+host = 'bandit.labs.overthewire.org'
+password = sys.argv[1]
+port = 2220
+
+shell = ssh(host = host, user = user, password = password, port = port)
+log.info("List dir: %s\n" % shell.ls(['-la']))
+log.success("Password: %s" % shell.cat(['spaces in this filename']))
+shell.close()
