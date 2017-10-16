5b8e95ef8a8af785e5b1a6ffd3176643a337e63a
DY <dy@Hmac.local>
2017-10-16 14:49:37 +0530

    Single script added

--- Bandit/bandit.py
+++ Bandit/bandit.py
@@ -3,35 +3,55 @@ from pwn import *
 
 def level0_to_level1(shell):
 	log.info("List dir: %s\n" % shell.ls(['-la']))
-	log.success("Flag: %s" % shell.cat(['readme']))
+	flag = shell.cat(['readme'])
+	log.success("Flag: %s" % flag)
 
 def level1_to_level2(shell):
 	log.info("List dir: %s\n" % shell.ls(['-la']))
-	log.success("Flag: %s" % shell.cat(['./-']))
+	flag = shell.cat(['./-'])
+	log.success("Flag: %s" % flag)
 
 def level2_to_level3(shell):
 	log.info("List dir: %s\n" % shell.ls(['-la']))
-	log.success("Flag: %s" % shell.cat(['spaces in this filename']))
+	flag = shell.cat(['spaces in this filename'])
+	log.success("Flag: %s" % flag)
 
 def level3_to_level4(shell):
 	log.info('inhere directory listing: %s\n' % shell.ls(['inhere', '-la']))
-	log.success('Flag: %s' % shell.cat(['inhere/.hidden']))
+	flag = shell.cat(['inhere/.hidden'])
+	log.success('Flag: %s' % flag)
 
 def level4_to_level5(shell):
 	log.info(shell.ls(['inhere', '-la']))
 	log.info(shell.find(['./inhere', '-type', 'f', '-exec', 'file', '{}', '+']))
-	log.success('Flag: %s' % shell.cat(['./inhere/-file07']))
+	flag = shell.cat(['./inhere/-file07'])
+	log.success('Flag: %s' % flag)
 
 def level5_to_level6(shell):
-	log.success('Flag: %s' % shell.find('inhere/ -type f -size 1033c -exec cat {} +'))
+	flag = shell.find('inhere/ -type f -size 1033c -exec cat {} +')
+	log.success('Flag: %s' % flag)
 
 def level6_to_level7(shell):
-	log.success('Flag: %s' % shell.find('/ -type f -group bandit6 -user bandit7 -size 33c -exec cat {} + 2> /dev/null'))
+	flag = shell.find('/ -type f -group bandit6 -user bandit7 -size 33c -exec cat {} + 2> /dev/null')
+	log.success('Flag: %s' % flag)
 
 def level7_to_level8(shell):
-	log.success('Flag: %s' % shell.cat('data.txt | grep millionth'))
+	flag = shell.cat('data.txt | grep millionth')
+	log.success('Flag: %s' % flag)
 
 def level8_to_level9(shell):
+	flag = shell.cat('data.txt | sort | uniq -u')
+	log.success('Flag: %s' % flag)
+	
+def level9_to_level10(shell):
+	flag = shell['strings data.txt | grep ==']
+	log.success('Flag:\n %s' % flag)
+
+def level10_to_level11(shell):	
+	flag = shell['base64 -d data.txt']
+	log.success('Flag: %s' % flag)
+
+def level11_to_level12(shell):
 	shell.interactive()
 
 def main():
@@ -65,8 +85,19 @@ def main():
 		level7_to_level8(shell)
 	elif username == 'bandit8':
 		level8_to_level9(shell)
+	elif username == 'bandit9':
+		level9_to_level10(shell)
+	elif username == 'bandit10':
+		level10_to_level11(shell)
+	elif username == 'bandit11':
+		level11_to_level12(shell)
 
 	shell.close()
+
+def god_mode(shell, host, username, password, port):
+	flag = level0_to_level1(shell)
+	shell = ssh(host = host, user = username, password = password, port = port)
+
 
 if __name__ == '__main__':
 	main()
